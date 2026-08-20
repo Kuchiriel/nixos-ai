fd915119cf835f92aff1d4e15ddb81aab862d9b4
Kuchiriel <matheus.almeida211094@gmail.com>
2026-08-20 07:46:25 -0300

    chore: update 1 file(s)

--- home-manager/modules/services/jarvis-wakeword.nix
+++ home-manager/modules/services/jarvis-wakeword.nix
@@ -61,7 +61,7 @@ let
   '';
 
   jarvisScript = pkgs.writers.writePython3Bin "jarvis-wakeword-daemon" {
-    flakeIgnore = [ "E501" ];
+    flakeIgnore = [ "E501" "E231" ];
     # libraries espera pacotes (ou função), não um env pronto — passar o env
     # resultava em PYTHONPATH vazio (import numpy falhava em runtime)
     libraries = (ps: with ps; [ numpy requests openwakewordPkg ]);
