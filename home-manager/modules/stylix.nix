cd75e08260a1d94e069cc9aaf4e7206d3f99a7a2
Kuchiriel <matheus.almeida211094@gmail.com>
2026-08-22 17:02:02 -0300

    chore: update 1 file(s)

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -29,6 +29,7 @@
       wofi.enable = false;
       hyprland.enable = false;
       hyprlock.enable = false;
+      foot.enable = false;  # Usamos nosso tema cyberpunk customizado
     };
 
     cursor = {
